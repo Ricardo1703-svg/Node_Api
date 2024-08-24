e322dfdac673a4c878512e683ebe8286ce1a3ec7
Ricardo1703-svg <ralvares961@gmail.com>
2024-08-23 19:26:40 -0600

    get

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 
-app.use(require('./routes/index'))
+app.get(require('./routes/index'))
 
 
 app.listen(4000)
