66dc78de9ea0cbdb5f33628bf7d33ac8e1e57db1
Ricardo1703-svg <ralvares961@gmail.com>
2024-08-23 18:33:16 -0600

    raiz 2

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 
-app.use(require('./src/routes/index'))
+app.use(require('./routes/index'))
 
 
 app.listen(4000)
