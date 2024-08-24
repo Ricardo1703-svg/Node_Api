a80657acbc8eb06013948d8a5fb0ebfbffcbcda5
Ricardo1703-svg <ralvares961@gmail.com>
2024-08-23 19:28:37 -0600

    denuevo

--- src/index.js
+++ src/index.js
@@ -4,7 +4,11 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 
-app.get(require('./routes/index'))
+app.use(require('./routes/index'))
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
 
 
 app.listen(4000)
